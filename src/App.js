43b2c19c75266c2af0c2b042d810dba9b354ccea
Dan <brown.danny.j@gmail.com>
2020-12-01 16:24:37 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { Select, FormControl, MenuItem } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import InfoBox from './InfoBox';
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+          const countries = data.map((country) => ({
+              name: country.country,
+              value: country.countryInfo.iso2,
+            }));
+
+            setCountries(countries);
+      })
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="app__header">
+      <h1>Covid-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          onChange={onCountryChange}
+          value={country}
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+
+          {
+            countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
+
+        </Select>
+      </FormControl>
+
+      </div>
+     
+      <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={123} total={2000}/>
+
+          <InfoBox title="Recovered" cases={123} total={2000}/>
+
+          <InfoBox title="Deaths" cases={123} total={2000}/>
+      </div>
+
+      {}
+      {}
+
+      {}
     </div>
   );
 }
